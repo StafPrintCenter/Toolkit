9ec65507c24c10ab3aa9a27434aea908f35b8c1a
steveasterafovo <steveasterafovo@gmail.com>
2026-09-12 17:50:54 +0100

    s

--- src/routes/_tool/tac-checker.tsx
+++ src/routes/_tool/tac-checker.tsx
@@ -1,7 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { useState } from "react";
-import { Field, Panel, Stat } from "@/components/spc/Layout";
-import { ToolkitShell } from "@/components/site";
+import { ToolkitShell, Field, Panel, Stat } from "@/components/site";
 import { getTool } from "@/data/toolsRegistry";
 import { Slider } from "@/components/ui/slider";
 import { Input } from "@/components/ui/input";
