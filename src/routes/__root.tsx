cad51bc559edbddd35fa570dce83e7d552a208bb
steveasterafovo <steveasterafovo@gmail.com>
2026-09-12 15:14:48 +0100

    q

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -28,7 +28,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
       { property: "og:image", content: `${logo.meta}` },
       { property: "og:image:width", content: "1200" },
       { property: "og:image:height", content: "630" },
-      { property: "og:image:alt", content: `SPC Interactive Brief - ${SITE.name}` },
+      { property: "og:image:alt", content: `SPC Creative Toolkit - ${SITE.name}` },
       { property: "og:url", content: `${SITE_LINK.briefUrl}` },
       { property: "og:locale", content: "fr_BJ" },
 
