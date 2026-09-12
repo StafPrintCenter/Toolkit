c9f9418ef76a182b370a2a2cb05f569a657fb948
steveasterafovo <steveasterafovo@gmail.com>
2026-09-12 15:29:02 +0100

    s

--- src/components/site/index.ts
+++ src/components/site/index.ts
@@ -1,5 +1,6 @@
-export { BriefHeader } from "./Header";
+export { ToolkitHeader } from "./Header";
 export { BriefFooter } from "./Footer";
 export { BriefShell } from "./BriefShell";
+export { ThemeToggle } from "./ThemeToggle";
 export { BriefSummary } from "./BriefSummary";
 export * from "./Primitives";
