fad33ea3a370f97798dcc5f138b1cb6370b4d47a
steveasterafovo <steveasterafovo@gmail.com>
2026-09-12 16:51:53 +0100

    s

--- src/components/site/Header.tsx
+++ src/components/site/Header.tsx
@@ -2,6 +2,7 @@ import { Link } from "@tanstack/react-router";
 import { Button } from "@/components/ui/button";
 import { ThemeToggle } from "./";
 import { SITE, SITE_LINK } from "@/data/site";
+import { SpcDeskLogo } from "@/components/site";
 import logo from "@/assets/logos.json";
 
 export function ToolkitHeader() {
@@ -9,7 +10,7 @@ export function ToolkitHeader() {
     <header className="sticky top-0 z-40 border-b border-border/70 bg-background/80 backdrop-blur-xl">
       <div className="mx-auto flex h-16 max-w-6xl items-center justify-between px-4">
         <Link to="/" className="flex items-center">
-          <img src={logo.dc} alt="Logo SPC" className="h-10 md:h-12 w-auto transition-all duration-300" />
+          <SpcDeskLogo className="mx-auto h-14 w-auto" />
         </Link>
 
         <div className="flex items-center gap-1">
