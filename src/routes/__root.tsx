21b8484b7c2ef55c90c261cedb016b7b597e3cbd
steveasterafovo <steveasterafovo@gmail.com>
2026-09-12 15:15:31 +0100

    s

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -43,7 +43,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
       { name: "google-site-verification", content: "OdKxHpVkBSxk0mj4vD4OTmZPdVi5pWzyCu4QPIMHy9A" },
     ],
     links: [
-      { rel: "canonical", href: `${SITE_LINK.briefUrl}` },
+      { rel: "canonical", href: `${SITE_LINK.toolkitUrl}` },
       { rel: "stylesheet", href: appCss, },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
