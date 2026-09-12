6c305014ef022596f31afbb791e0227f5c9a6f17
steveasterafovo <steveasterafovo@gmail.com>
2026-09-12 17:51:43 +0100

    s

--- src/routes/_tool/pdf-tools.tsx
+++ src/routes/_tool/pdf-tools.tsx
@@ -1,8 +1,7 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { useState } from "react";
 import { FileText, Loader2, Trash2, Upload } from "lucide-react";
-import { Field, Panel } from "@/components/spc/Layout";
-import { ToolkitShell } from "@/components/site";
+import { ToolkitShell, Field, Panel } from "@/components/site";
 import { getTool } from "@/data/toolsRegistry";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
