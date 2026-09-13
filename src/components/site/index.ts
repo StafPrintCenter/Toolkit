54c0f0e4e790c12e2b3dc8193e9e048f311f44a4
steveasterafovo <steveasterafovo@gmail.com>
2026-09-13 22:01:32 +0100

    s

--- src/components/site/index.ts
+++ src/components/site/index.ts
@@ -2,6 +2,5 @@ export { ToolkitHeader } from "./Header";
 export { ToolkitFooter } from "./Footer";
 export { ToolkitShell } from "./ToolkitShell";
 export { ThemeToggle } from "./ThemeToggle";
-export { CtaBanner } from "./CtaBanner";
 export { SpcMobLogo, SpcDeskLogo } from "./SpcLogo";
 export * from "./Layout";
