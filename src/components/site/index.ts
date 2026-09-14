baf3b69389ee54f59381a26062a9cae9599ca5bc
steveasterafovo <steveasterafovo@gmail.com>
2026-09-14 11:39:27 +0100

    s

--- src/components/site/index.ts
+++ src/components/site/index.ts
@@ -1,6 +1,7 @@
 export { ToolkitHeader } from "./Header";
 export { ToolkitFooter } from "./Footer";
 export { ToolkitShell } from "./ToolkitShell";
+export { CookieConsent } from "./CookieConsent";
 export { ThemeToggle } from "./ThemeToggle";
 export { SpcMobLogo, SpcDeskLogo } from "./SpcLogo";
 export { CtaBanner } from "./CtaBanner";
