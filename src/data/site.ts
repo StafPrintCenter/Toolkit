6d1faa18aa38f59705eebf47c438a34ddd25182c
steveasterafovo <steveasterafovo@gmail.com>
2026-09-15 14:41:37 +0100

    c

--- src/data/site.ts
+++ src/data/site.ts
@@ -1,5 +1,6 @@
 export const SITE = {
   name: "STAF PRINT CENTER",
+  tool: "SPC Creative Toolkit",
   slogan: "L'empreinte de votre succès",
   activity: "Studio de design et d'impression",
   manager: "Steve Aster Afovo",
