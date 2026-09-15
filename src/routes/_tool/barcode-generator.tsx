78457e5c2a2ee94fa1d0f8f98edec55aad1eded2
steveasterafovo <steveasterafovo@gmail.com>
2026-09-15 14:41:25 +0100

    n

--- src/routes/_tool/barcode-generator.tsx
+++ src/routes/_tool/barcode-generator.tsx
@@ -7,7 +7,7 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { SITE } from "@/data/site";
 
-const PAGE_TITLE = `Générateur de QR codes et codes-barres vectoriels | SPC Toolkit | ${SITE.name}`;
+const PAGE_TITLE = `Générateur de QR codes et codes-barres vectoriels | ${SITE.tool} | ${SITE.name}`;
 const PAGE_DESC = `Créez des QR codes avec logo central et des codes-barres EAN-13 ou Code 128, exportables en SVG et PDF pour l'impression.`;
 
 const tool = getTool("/barcode-generator")!;
