3d46acd48a8a447dcf2fade8615a3fef3b5f3787
steveasterafovo <steveasterafovo@gmail.com>
2026-09-18 01:03:50 +0100

    s

--- src/components/site/CookieConsent.tsx
+++ src/components/site/CookieConsent.tsx
@@ -2,8 +2,8 @@ import { useEffect, useState } from "react";
 import { Cookie } from "lucide-react";
 import { SITE_LINK } from "@/data/site";
 
-const STORAGE_KEY = "spc_roadmap_cookie_consent_v1";
-const GA_ID = "G-2N5DXSES7M";
+const STORAGE_KEY = "spc_toolkit_cookie_consent_v1";
+const GA_ID = "G-MJ47YJ4V60";
 
 type Consent = "accepted" | "declined";
 
