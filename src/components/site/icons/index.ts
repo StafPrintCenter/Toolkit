10885dd83066d34cab7c2a20ddf5446d48a905b7
steveasterafovo <steveasterafovo@gmail.com>
2026-09-21 10:55:53 +0100

    q

---
+++ src/components/site/icons/index.ts
@@ -0,0 +1,5 @@
+export { FacebookIcon } from "./FacebookIcon";
+export { InstagramIcon } from "./InstagramIcon";
+export { WhatsAppIcon } from "./WhatsAppIcon";
+export { LinkedinIcon } from "./LinkedinIcon";
+export { XIcon } from "./XIcon";
