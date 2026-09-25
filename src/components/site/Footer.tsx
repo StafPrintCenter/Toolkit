d5d57a7d3dda1ca6394dc48329b2a6f1f81351e8
steveasterafovo <steveasterafovo@gmail.com>
2026-09-25 15:45:44 +0100

    s

--- src/components/site/Footer.tsx
+++ src/components/site/Footer.tsx
@@ -6,7 +6,6 @@ export function ToolkitFooter() {
     { label: "LinkedIn", href: SITE.socials.linkedin, Icon: LinkedinIcon },
     { label: "Facebook", href: SITE.socials.facebook, Icon: FacebookIcon },
     { label: "Instagram", href: SITE.socials.instagram, Icon: InstagramIcon },
-    { label: "LinkedIn", href: SITE.socials.linkedin, Icon: LinkedinIcon },
     { label: "X", href: SITE.socials.x, Icon: XIcon },
     { label: "WhatsApp", href: SITE.whatsappLink, Icon: WhatsAppIcon },
   ];
