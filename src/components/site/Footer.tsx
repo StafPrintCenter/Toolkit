914d023e3d4ef9b564268387a0c6a6de9525dcfd
steveasterafovo <steveasterafovo@gmail.com>
2026-09-25 15:45:39 +0100

    s

--- src/components/site/Footer.tsx
+++ src/components/site/Footer.tsx
@@ -3,6 +3,7 @@ import { FacebookIcon, InstagramIcon, LinkedinIcon, XIcon, WhatsAppIcon } from "
 
 export function ToolkitFooter() {
   const socialLinks = [
+    { label: "LinkedIn", href: SITE.socials.linkedin, Icon: LinkedinIcon },
     { label: "Facebook", href: SITE.socials.facebook, Icon: FacebookIcon },
     { label: "Instagram", href: SITE.socials.instagram, Icon: InstagramIcon },
     { label: "LinkedIn", href: SITE.socials.linkedin, Icon: LinkedinIcon },
